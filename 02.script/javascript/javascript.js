a34df49f4aaa56559d9cf5642648a5359c515c84
ikgoes <seogki.k@gmail.com>
2023-07-05 11:39:31 +0900

    update

--- 02.script/javascript/javascript.js
+++ 02.script/javascript/javascript.js
@@ -1,6 +1,7 @@
 
 //---------------------------------------------------#
 // 1시간에 한번씩 주기적으로 ClassName으로 찾은 Elements의 배열의 첫번째 항목 클릭하는 역할
+
 setInterval(()=>{
     document.getElementsByClassName('virtualized-table-csv-export-button')[0].click()
 }, 1000*60*60);
--- 02.script/powershell/powershell.ps1
+++ 02.script/powershell/powershell.ps1
@@ -6,10 +6,15 @@ $d1 = Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object LastBootU
 $d2 = Get-Date
 $d1.LastBootUpTime -lt $d2.AddDays(-1)
 $d1.LastBootUpTime -lt $d2.AddHours(-1)
+
 #---------------------------------------------------#
 # Add DNS A name record
 
 Add-DnsServerResourceRecordA -Name "mgmt_dellsw1" -ZoneName "dvc.lgensol.com" -AllowUpdateAny -IPv4Address "10.94.41.151"  -CreatePtr
 Add-DnsServerResourceRecordA -Name "gwINTERNET3" -ZoneName "dvc.lgensol.com" -AllowUpdateAny -IPv4Address "10.94.40.29"  -CreatePtr
+Add-DnsServerResourceRecordA -Name "igw01" -ZoneName "uoh.dvc.lgensol.com" -AllowUpdateAny -IPv4Address "192.168.60.173"  -CreatePtr
+Add-DnsServerResourceRecordA -Name "igw02" -ZoneName "uoh.dvc.lgensol.com" -AllowUpdateAny -IPv4Address "192.168.60.174"  -CreatePtr
+Add-DnsServerResourceRecordA -Name "igw03" -ZoneName "uoh.dvc.lgensol.com" -AllowUpdateAny -IPv4Address "192.168.60.175"  -CreatePtr
+
 
 #---------------------------------------------------#
