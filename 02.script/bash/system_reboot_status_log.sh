d4f673804c962c627f0d66423ef27320f38aa56f
ikgoes <68369277+ikgoes@users.noreply.github.com>
2025-02-13 11:16:53 +0900

    Create system_reboot_status_log.sh

---
+++ 02.script/bash/system_reboot_status_log.sh
@@ -0,0 +1,4 @@
+netstat -rn >> /var/tmp/system_status_250212_2100.log 
+df -Th >> /var/tmp/system_status_250212_2100.log 
+lsblk >> /var/tmp/system_status_250212_2100.log 
+ip a >> /var/tmp/system_status_250212_2100.log 
